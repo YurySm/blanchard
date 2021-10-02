9795f47bfc7080da2ebbe8a7aee6958f22a01f4b
Your Name <you@example.com>
2021-10-03 00:17:07 +0300

    add promo slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    //cusom selects
     const useCustomSelect = (el) => {
         if( typeof el ===  'string'){
             const element = document.querySelector(el);
@@ -18,4 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const listSelects = document.querySelectorAll('.promo__selects > li > select');
     listSelects.forEach(item => useCustomSelect(item));
+
+    //slider 
+    $('.promo__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
